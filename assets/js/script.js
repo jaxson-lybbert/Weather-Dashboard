69f23c7f3e623c08453a5614a9fc99f89751087b
jaxson-lybbert <jax.a.lybb@gmail.com>
2023-05-30 16:53:05 -0400

    "Added comments to script.js about API key and calls"

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+// API call for 5 day forecast: api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+// API call to get LAT/LON coordinates for city name: http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+
+// API Key: 25adc732fcb0ebbfd462bcfae063f791
