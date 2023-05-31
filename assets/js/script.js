df8ca0d1cdbd6988865376d724e9ff28be453663
jaxson-lybbert <jax.a.lybb@gmail.com>
2023-05-31 19:26:48 -0400

    Edit API call comments and practiced fetching API data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// API call for 5 day forecast: api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+// API call for 5 day forecast: http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 // API call to get LAT/LON coordinates for city name: http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 
 // API Key: 25adc732fcb0ebbfd462bcfae063f791
@@ -6,6 +6,7 @@
 
 var submitEl = $("#submit-button");
 var forecastEl = $("#forecast");
+var searchHistoryEl = $("button");
 
 // Stores city name to local storage when the submit button is clicked and adds to recently searched cities list
 submitEl.on("click", function (e) {
@@ -18,9 +19,5 @@ submitEl.on("click", function (e) {
   var recentCityButton = $("<button>");
   recentCityButton.addClass("recent-search");
   recentCityButton.text(citySearch);
-  if ($("aside").children(0) == true) {
-    $("button").append(recentCityButton);
-  } else {
-    $("aside").append(recentCityButton);
-  }
+  $("aside").append(recentCityButton);
 });
